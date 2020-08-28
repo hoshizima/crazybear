79744eca897e4a34a2d929b67ceb760e72cafc7f
hoshizima <none>
2020-08-28 21:04:46 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,50 +1,59 @@
+const GAMESCENEWIDTH=320;
+const GAMESCENEHEIGHT=320;
+const CHARACTORWIDTH=32;
+const CHARACTORHEIGHT=32;
+const CHARACTORFIRST_X=0;
+const CHARACTORFIRST_Y=0;
+
 enchant();
 
 window.onload = function () {
-  var game = new Game(320, 320);
-  game.fps = 30;
+  var game = new Game(GAMESCENEWIDTH, GAMESCENEHEIGHT);
+  game.fps = 60;
   game.preload("./img/chara1.png");
-  var score = 0;
-  var scoremessage = new Label("score:");
-  var touchmessage = new Label("Touch the crazy bear!!");
+  var scoremessage = new Label("up:↑ down:↓ left:← right:→");
   scoremessage.font = "16px Palatino";
   scoremessage.x = 10;
   scoremessage.y = 5;
-  touchmessage.x = 10;
-  touchmessage.y = 30;
 
   game.rootScene.addChild(scoremessage);
-  game.rootScene.addChild(touchmessage);
 
   game.onload = function () {
-    var kuma = new Sprite(32, 32);
+    var kuma = new Sprite(CHARACTORWIDTH, CHARACTORWIDTH);
     kuma.image = game.assets["./img/chara1.png"];
-    kuma.x = 100;
-    kuma.y = 120;
+    kuma.x = CHARACTORFIRST_X;
+    kuma.y = CHARACTORFIRST_Y;
     game.rootScene.addChild(kuma);
     game.rootScene.backgroundColor = "#7ecef4";
-    setInterval(function () {
-      kuma.tl.moveTo(Math.random() * 320, Math.random() * 320, 30);
-    }, 1000);
+
+  
     //フレームごとに実行
     game.rootScene.addEventListener(Event.ENTER_FRAME, function () {
       kuma.frame++;
       if (kuma.frame === 3) {
         kuma.frame = 0;
       }
-
-      scoremessage.text = "score:" + score;
-    });
-    // タッチイベント時に実行
-    kuma.addEventListener(Event.TOUCH_START, function (e) {
-      if (
-        e.x > kuma.x &&
-        e.x < kuma.x + 32 &&
-        e.y > kuma.y &&
-        e.y < kuma.y + 32
-      ) {
-        score++;
-      }
+          //キー入力を受け付ける。
+  if(kuma.y>0){
+    if(game.input.up){
+      kuma.y-=32;
+    }
+  }
+  if(kuma.y<GAMESCENEHEIGHT-CHARACTORHEIGHT){
+    if(game.input.down){
+      kuma.y+=32;
+    }
+  }
+  if(kuma.x>0){
+    if(game.input.left){
+      kuma.x-=32;
+    }
+  }
+  if(kuma.x<GAMESCENEWIDTH-CHARACTORWIDTH){
+    if(game.input.right){
+      kuma.x+=32;
+    }
+  }
     });
   };
   game.start();

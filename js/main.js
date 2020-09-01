f1b170fd0e3c29544176601110fea20ac13fa4a5
hoshizima <none>
2020-09-02 00:58:19 +0900

    画像の変更及びリボンの動作の設定を追加

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 /*定数定義*/
-const coreSCENEWIDTH = 320;
-const coreSCENEHEIGHT = 320;
-const CHARACTORWIDTH = 32;
-const CHARACTORHEIGHT = 32;
+const coreSCENEWIDTH = 640;
+const coreSCENEHEIGHT = 640;
+const CHARACTORWIDTH = 64;
+const CHARACTORHEIGHT = 64;
 const CHARACTORFIRST_X = 0;
 const CHARACTORFIRST_Y = 0;
 const CHARACTORHP = 100;
 const CHARACTORAP = 100;
+const MOVE_DISTANCE = 64;
 
 /*メイン処理開始*/
 enchant();
 
-window.onload = function () {
+window.onload = function ()
+{
   var core = new Core(coreSCENEWIDTH, coreSCENEHEIGHT);
   core.fps = 60;
-  core.preload("./img/chara1.png");
+  core.preload("./img/cellgirl_body.png");
+  core.preload("./img/cellgirl_ribbon.png");
   var scoremessage = new Label("up:↑ down:↓ left:← right:→");
   scoremessage.font = "16px Palatino";
   scoremessage.x = 10;
@@ -23,52 +26,81 @@ window.onload = function () {
 
   core.rootScene.addChild(scoremessage);
 
-  core.onload = function () {
+  core.onload = function ()
+  {
     var kuma = new Sprite(CHARACTORWIDTH, CHARACTORWIDTH);
-    kuma.image = core.assets["./img/chara1.png"];
+    var ribbon = new Sprite(CHARACTORWIDTH, CHARACTORWIDTH);
+    kuma.image = core.assets["./img/cellgirl_body.png"];
+    ribbon.image = core.assets["./img/cellgirl_ribbon.png"];
     kuma.x = CHARACTORFIRST_X;
     kuma.y = CHARACTORFIRST_Y;
+    ribbon.x = CHARACTORFIRST_X;
+    ribbon.y = CHARACTORFIRST_Y;
     kuma.hp = CHARACTORHP;
     kuma.ap = CHARACTORAP;
     core.rootScene.addChild(kuma);
+    core.rootScene.addChild(ribbon);
     core.rootScene.backgroundColor = "#7ecef4";
 
-    core.rootScene.addEventListener(Event.UP_BUTON_DOWN, function () {
+    core.rootScene.addEventListener(Event.UP_BUTON_DOWN, function ()
+    {
       console.log("ok");
     });
 
     //フレームごとに実行
-    core.rootScene.addEventListener(Event.ENTER_FRAME, function () {
-      if (core.frame % core.fps == 0) {
+    core.rootScene.addEventListener(Event.ENTER_FRAME, function ()
+    {
+      if (core.frame % (core.fps / 2) == 0)
+      {
         kuma.frame++;
       }
-      if (kuma.frame === 3) {
+      if (core.frame % (core.fps / 3) == 0)
+      {
+        ribbon.frame++;
+      }
+      if (kuma.frame === 3)
+      {
         kuma.frame = 0;
       }
+      if (ribbon.frame === 3)
+      {
+        ribbon.frame = 0;
+      }
       //キー入力を受け付ける。
-      if (kuma.y > 0) {
-        if (core.input.up) {
-          kuma.y -= 32;
+      if (kuma.y > 0)
+      {
+        if (core.input.up)
+        {
+          kuma.y -= MOVE_DISTANCE;
         }
       }
-      if (kuma.y < coreSCENEHEIGHT - CHARACTORHEIGHT) {
-        if (core.input.down) {
-          kuma.y += 32;
+      if (kuma.y < coreSCENEHEIGHT - CHARACTORHEIGHT)
+      {
+        if (core.input.down)
+        {
+          kuma.y += MOVE_DISTANCE;
         }
       }
-      if (kuma.x > 0) {
-        if (core.input.left) {
-          kuma.x -= 32;
+      if (kuma.x > 0)
+      {
+        if (core.input.left)
+        {
+          kuma.x -= MOVE_DISTANCE;
         }
       }
-      if (kuma.x < coreSCENEWIDTH - CHARACTORWIDTH) {
-        if (core.input.right) {
-          kuma.x += 32;
+      if (kuma.x < coreSCENEWIDTH - CHARACTORWIDTH)
+      {
+        if (core.input.right)
+        {
+          kuma.x += MOVE_DISTANCE;
           console.log("kuma_x:" + kuma.x);
           showProps(core, "core");
           showProps(kuma, "kuma");
         }
       }
+      //リボンの位置を調整
+      ribbon.x = kuma.x;
+      ribbon.y = kuma.y;
     });
   };
   core.start();
@@ -78,11 +110,14 @@ window.onload = function () {
 https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Working_with_Objects
 オブジェクト変数を引数に入れることでオブジェクト内のプロパティを表示する。
 */
-function showProps(obj, objName) {
+function showProps(obj, objName)
+{
   var result = "";
-  for (var i in obj) {
+  for (var i in obj)
+  {
     // obj.hasOwnProperty() はオブジェクトのプロトタイプチェーンからプロパティを絞り込むために使用しています
-    if (obj.hasOwnProperty(i)) {
+    if (obj.hasOwnProperty(i))
+    {
       result += objName + "." + i + " = " + obj[i] + "\n";
     }
   }

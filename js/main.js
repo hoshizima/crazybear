f7c5c75660752b63ecacd69df42c310d90356f3f
hoshizima <ko.0027.oshim@outlook.com>
2020-09-18 02:01:30 +0900

    html要素の追加及びレイアウトの変更（中央寄せ）

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ const CHARACTORHP = 100;
 const CHARACTORAP = 100;
 const MOVE_DISTANCE_X = 46;
 const MOVE_DISTANCE_Y = 34;
-
 var charactorappearance_x = [];
 var charactorappearance_y = [];
 var enemies = [];
@@ -21,7 +20,11 @@ var enemies = [];
 /*メイン処理開始*/
 enchant();
 
-window.onload = function () {
+window.onload = function ()
+{
+  /*HTML内要素を取得 */
+  var hitpoint = document.getElementById("hitpoint");
+  hitpoint.innerHTML = "HP：" + CHARACTORHP;
   /*ゲームオブジェクトの作成、画像のプリロード*/
   var core = new Core(coreSCENEWIDTH, coreSCENEHEIGHT);
   core.fps = 30;
@@ -36,7 +39,8 @@ window.onload = function () {
   context.beginPath();
   context.strokeStyle = "rgb(255, 255, 255)";
   context.lineWidth = 1;
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 20; i++)
+  {
     charactorappearance_x[i] = i * CHARACTORREALWIDTH;
     charactorappearance_y[i] = i * CHARACTORREALHEIGHT;
     context.moveTo(charactorappearance_x[i], 0);
@@ -48,13 +52,13 @@ window.onload = function () {
   }
   core.rootScene.addChild(background);
 
-  /*ＨＰ表示 */
-  var hplabel = new Label("HP:100");
-  hplabel.font = "32px Palatino";
-  hplabel.color = "#ffff00";
-  hplabel.x = 10;
-  hplabel.y = 10;
-  core.rootScene.addChild(hplabel);
+  /*canvas内ＨＰ表示 */
+  // var hplabel = new Label("HP:100");
+  // hplabel.font = "32px Palatino";
+  // hplabel.color = "#ffff00";
+  // hplabel.x = 10;
+  // hplabel.y = 10;
+  // core.rootScene.addChild(hplabel);
 
   /*キャラクターの初期化フラグ*/
   var initializationflag = false;
@@ -68,7 +72,8 @@ window.onload = function () {
   core.keybind(65, "a");
   core.keybind(67, "c");
 
-  core.onload = function () {
+  core.onload = function ()
+  {
     var kuma = new Sprite(CHARACTORWIDTH, CHARACTORWIDTH);
     var ribbon = new Sprite(CHARACTORWIDTH, CHARACTORWIDTH);
     var enemies = (kuma.image = core.assets["./img/cellgirl_body.png"]);
@@ -84,52 +89,67 @@ window.onload = function () {
     core.rootScene.addChild(ribbon);
 
     //kumaへイベント処理を追加
-    kuma.addEventListener(Event.ENTER_FRAME, function actions() {
-      if (initializationflag) {
+    kuma.addEventListener(Event.ENTER_FRAME, function actions()
+    {
+      if (initializationflag)
+      {
         kuma.scaleX = kuma.scaleY = ribbon.scaleX = ribbon.scaleY = 1;
       }
-      if (core.frame % (core.fps / 2) == 0) {
+      if (core.frame % (core.fps / 2) == 0)
+      {
         this.frame++;
       }
-      if (core.frame % (core.fps / 3) == 0) {
+      if (core.frame % (core.fps / 3) == 0)
+      {
         ribbon.frame++;
       }
       this.frame %= 3;
       ribbon.frame %= 3;
       //移動する
-      if (this.isMoving) {
+      if (this.isMoving)
+      {
         this.moveBy(this.vx, this.vy);
         this.isMoving = false;
-      } else {
+      } else
+      {
         //移動距離の初期化
         this.vy = this.vx = 0;
         //キー入力を取得し移動を決定
-        if (core.input.down) {
+        if (core.input.down)
+        {
           this.vy = MOVE_DISTANCE_Y;
         }
-        if (core.input.up) {
+        if (core.input.up)
+        {
           this.vy = -MOVE_DISTANCE_Y;
         }
-        if (core.input.left) {
+        if (core.input.left)
+        {
           this.vx = -MOVE_DISTANCE_X;
         }
-        if (core.input.right) {
+        if (core.input.right)
+        {
           this.vx = MOVE_DISTANCE_X;
         }
 
-        if (core.input.shift) {
-          if (core.input.space) {
+        if (core.input.shift)
+        {
+          if (core.input.space)
+          {
             //shift+spaceの処理
             core.rootScene.backgroundColor = "#ffffff";
           }
         }
 
-        if (core.input.ctrl) {
-          if (core.input.space) {
+        if (core.input.ctrl)
+        {
+          if (core.input.space)
+          {
             //ctrl+spaceの処理
             core.rootScene.backgroundColor = "#000000";
           }
-          if (core.input.a) {
+          if (core.input.a)
+          {
             //ctrl+aの処理
             kuma.scaleX *= 10;
             kuma.scaleY *= 10;
@@ -139,7 +159,8 @@ window.onload = function () {
           }
         }
 
-        if (this.vy || this.vx) {
+        if (this.vy || this.vx)
+        {
           this.isMoving = true;
           actions();
         }
@@ -149,12 +170,15 @@ window.onload = function () {
       ribbon.y = this.y;
     }); //kuma.addEventListener終了
 
-    setInterval(() => {
+    setInterval(() =>
+    {
       var enemy = createEnemy();
       core.rootScene.addChild(enemy);
-      enemy.addEventListener(Event.ENTER_FRAME, function () {
+      enemy.addEventListener(Event.ENTER_FRAME, function ()
+      {
         enemy.x += enemy.speed;
-        if (enemy.x > 640) {
+        if (enemy.x > 640)
+        {
           core.rootScene.removeChild(enemy);
         }
         if (
@@ -162,11 +186,12 @@ window.onload = function () {
           enemy.y + enemy.hitbox_y > kuma.y &&
           enemy.x < kuma.x + CHARACTORREALWIDTH &&
           enemy.y < kuma.y + CHARACTORREALHEIGHT
-        ) {
+        )
+        {
           core.rootScene.removeChild(enemy);
           kuma.hp -= 1;
-          hplabel.text = "HP:" + kuma.hp;
-          core.rootScene.addChild(hplabel);
+          hitpoint.innerHTML = "HP:" + kuma.hp;
+          //core.rootScene.addChild(hplabel);
         }
       });
     }, 100);
@@ -177,7 +202,8 @@ window.onload = function () {
 /**
  * 敵オブジェクトを作成します
  */
-function createEnemy() {
+function createEnemy()
+{
   var enemy = new Label("密");
   enemy.font = "32px Palatino";
   enemy.color = "#ffffff";
@@ -201,11 +227,14 @@ function createEnemy() {
  * オブジェクト変数を引数に入れることでオブジェクト内のプロパティを表示する。
  */
 
-function showProps(obj, objName) {
+function showProps(obj, objName)
+{
   var result = "";
-  for (var i in obj) {
+  for (var i in obj)
+  {
     // obj.hasOwnProperty() はオブジェクトのプロトタイプチェーンからプロパティを絞り込むために使用しています
-    if (obj.hasOwnProperty(i)) {
+    if (obj.hasOwnProperty(i))
+    {
       result += objName + "." + i + " = " + obj[i] + "\n";
     }
   }
---
+++ memo/sanko.md
@@ -0,0 +1,2 @@
+enchant-stage idの使い方、
+http://tatyusa.logdown.com/posts/155836/enchant-stage
